e8b63787a3296ebc0e3df06e4a61dbea95d5de7e
MarcoPadeiroIPL <2211868@my.ipleiria.pt>
2022-11-18 00:06:10 +0000

    Fix script to install nvim config

--- nvim.sh
+++ nvim.sh
@@ -2,9 +2,11 @@
 # This is a script to install my config of nvim in any pc
 
 echo "Installing Packer..."
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+rm -rf $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim &> /dev/null
 
 echo "Linking current config to the right path..."
+mv $HOME/.config/nvim $HOME/.config/old_nvim
 ln -s $PWD/nvim $HOME/.config
 
 echo "Syncing packages..."
