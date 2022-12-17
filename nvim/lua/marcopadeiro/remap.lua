7eb95a83dabca0d226628ec32bfc0fa409f5a05e
MarcoPadeiroIPL <2211868@my.ipleiria.pt>
2022-12-17 18:10:33 +0000

    Changing the way LSP works

--- nvim/lua/marcopadeiro/remap.lua
+++ nvim/lua/marcopadeiro/remap.lua
@@ -1,4 +1,5 @@
 local nnoremap = require("marcopadeiro.keymap").nnoremap
+local vnoremap = require("marcopadeiro.keymap").vnoremap
 -- Telescope
 nnoremap("<leader><leader>", "<cmd>Telescope find_files<CR>")
 
@@ -12,6 +13,20 @@ nnoremap("<leader>k", ':lua require("harpoon.ui").nav_file(3)<CR>')
 nnoremap("<leader>l", ':lua require("harpoon.ui").nav_file(4)<CR>')
 nnoremap("<leader>m", ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
 nnoremap("<leader>a", ':lua require("harpoon.mark").add_file()<CR>')
+
+vnoremap("J", ":m '>+1<CR>gv=gv")
+vnoremap("K", ":m '<-2<CR>gv=gv")
+
+-- next greatest remap ever : asbjornHaland
+nnoremap("<leader>y", "\"+y")
+vnoremap("<leader>y", "\"+y")
+nnoremap("<leader>Y", "\"+Y")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
 
 -- Better window navigation
 nnoremap("<C-h>", "<C-w>h")
